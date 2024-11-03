515e4d7d9d207b499c932f09a4511a2e9fa556fe
Seth <sethgho@gmail.com>
2024-11-02 21:53:16 -0500

    Metadata

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,9 +5,44 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Skate time notifications at Ice & Field in Cedar Park, TX"
+        />
+        <meta name="theme-color" content="#049BDF" />
+        <meta property="og:title" content="IceTime Alerts" />
+        <meta
+          property="og:description"
+          content="Skate time notifications at Ice & Field in Cedar Park, TX"
+        />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="IceTime Alerts" />
+        <meta
+          name="twitter:description"
+          content="Skate time notifications at Ice & Field in Cedar Park, TX"
+        />
         <title>IceTime Alerts</title>
         <link rel="stylesheet" href="/styles.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"

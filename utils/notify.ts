b416153e5d6120815c27b27b33c1dd5c61125d51
Seth <sethgho@gmail.com>
2024-11-03 08:22:24 -0600

    Yay CI troubleshooting

--- utils/notify.ts
+++ utils/notify.ts
@@ -18,7 +18,8 @@ import { ICETIME_ALERTS_BASE_URL } from "./config.ts";
 
 // Something funky happens during the build process with deno deploy.
 // https://github.com/denoland/fresh/issues/1843
-if (VAPID_PRIVATE_KEY) {
+console.log("WEFWEF", CONTACT_EMAIL);
+if (CONTACT_EMAIL && VAPID_PUBLIC_KEY && VAPID_PRIVATE_KEY) {
     // Configure web-push
     webPush.setVapidDetails(
         CONTACT_EMAIL,
